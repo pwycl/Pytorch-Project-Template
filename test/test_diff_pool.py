c2697fbddd82ad7a6795e87339fb95bbb9b652a1
pwycl <814821479@qq.com>
2019-08-03 11:35:41 +0800

    Update test_diff_pool.py

--- test/test_diff_pool.py
+++ test/test_diff_pool.py
@@ -41,14 +41,10 @@ def test_Diff_Pool():
 	model=DiffPool(dataset,config.num_layers,config.hidden)
 	model.eval()
 	for batch in smt.train_loader:
-		assert list(batch.x.size())[1:]==[smt.num_nodes,dataset[0].num_features]
+		assert list(batch.x.size())[:]==[config.batch_size,smt.num_nodes,dataset[0].num_features]
 		assert list(batch.adj.size())[1:]==[smt.num_nodes,smt.num_nodes]
 		assert list(batch.mask.size())[1:]==[smt.num_nodes]
 		out=model(batch)
 		assert list(out.size())[1:]==[dataset.num_classes]
 
-if __name__ == '__main__':
-	test_pyg_diff_pool()
-	test_Diff_Pool()
-	print('finished')
 
