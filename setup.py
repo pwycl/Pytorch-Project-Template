d13100d772bf900be74c6f77dcbb3660c274a3fc
pwycl <814821479@qq.com>
2019-08-03 10:25:21 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='PygDiffPool',
+	  version='0.1',
+	  description='DiffPool in PyG',
+	  # auto='pwy',
+	  # author_email='814821479@qq.com',
+	  # url='https://github.com/pwycl/Pytorch-Project-Template',
+	  packages=find_packages()
+	  )
