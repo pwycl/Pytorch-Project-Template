0447ed313b6114abfd4206c96d6b80569c0fe8c5
pwycl <814821479@qq.com>
2019-08-05 16:50:53 +0800

    Update SMT.py

--- datasets/SMT.py
+++ datasets/SMT.py
@@ -11,17 +11,6 @@ from torch_geometric.datasets import TUDataset
 from torch_geometric.data import DataLoader, DenseDataLoader, DataListLoader
 import torch_geometric.transforms as T
 
-#####test#####
-
-# from easydict import EasyDict
-# config_dict={
-# 	'batch_size': 24,
-# 	'sparse': True,
-# 	'dataset_div': 10,
-# }
-# config=EasyDict(config_dict)
-#####test#####
-
 class SMTDataLoader:
 	def __init__(self, config):
 		self.config=config
@@ -74,8 +63,42 @@ class SMTDataLoader:
 
 		return dataset
 
+	def get_k_fold_indices(self, folds,len_dataset):
+		from sklearn.model_selection import StratifiedKFold
+		skf=StratifiedKFold(folds,shuffle=True,random_state=12345)
+		
+		test_indices, train_indices=[],[]
+		for _, idx in skf.split(torch.zeros(len_dataset),torch.zeros(len_dataset)):
+			test_indices.append(torch.from_numpy(idx))
 
+		val_indices=[test_indices[i-1] for i in range(folds)]
 
+		for i in range(folds):
+			train_mask=torch.ones(len_dataset, dtype=torch.uint8)
+			train_mask[test_indices[i]]=0
+			train_mask[val_indices[i]]=0
+			train_indices.append(train_mask.nonzero().view(-1))
+
+		return train_indices, val_indices, test_indices
+		"""
+		# test: assert isinstance(train_indices[i])==torch.tensor 
+		
+
+		"""
+
+	# using generator to yield the train/val/test_loader 
+	def k_fold_loader_generater(self,folds):
+		dataset=self.get_dataset('SMT',sparse=self.config.sparse, dataset_div=self.config.dataset_div)
+		train_indices,val_indices,test_indices=self.get_k_fold_indices(folds,len(dataset))
+
+		for fold, (train_idx,val_idx,test_idx) in enumerate(
+				zip(train_indices,val_indices,test_indices)):
+
+			train_loader=DenseDataLoader(dataset[train_idx],self.config.batch_size,shuffle=True)
+			val_loader=DenseDataLoader(dataset[val_idx],self.config.batch_size,shuffle=False)
+			test_loader=DenseDataLoader(dataset[test_idx],self.config.batch_size,shuffle=False)
+
+			yield train_loader,val_loader,test_loader
 
 
 
