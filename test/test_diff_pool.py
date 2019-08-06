cd3a3c69b4d247c8a579abfb56601491008af9a6
pwycl <814821479@qq.com>
2019-08-06 15:57:41 +0800

    Update test_diff_pool.py

--- test/test_diff_pool.py
+++ test/test_diff_pool.py
@@ -11,34 +11,34 @@ config_dict={
 	'dataset_div':10,
 	'num_nodes':5187,
 	'channels':128,
-	'num_layers':5,
+	'num_pools':5,
 	'hidden':64,
 }
 config=EasyDict(config_dict)
 config.num_clusters=ceil(config.num_nodes*0.25)
 
-# def test_pyg_diff_pool():
-# 	batch_size,num_nodes,channels,num_clusters=(
-# 					config.batch_size,config.num_nodes,config.channels,config.num_clusters)
+def test_pyg_diff_pool():
+	batch_size,num_nodes,channels,num_clusters=(
+					config.batch_size,config.num_nodes,config.channels,config.num_clusters)
 
-# 	x=torch.randn((batch_size,num_nodes,channels))
-# 	adj=torch.rand((batch_size,num_nodes,num_nodes))
-# 	s=torch.randn((batch_size,num_nodes,num_clusters))
-# 	mask=torch.randint(0,1,(batch_size,num_nodes),dtype=torch.uint8)
+	x=torch.randn((batch_size,num_nodes,channels))
+	adj=torch.rand((batch_size,num_nodes,num_nodes))
+	s=torch.randn((batch_size,num_nodes,num_clusters))
+	mask=torch.randint(0,1,(batch_size,num_nodes),dtype=torch.uint8)
 
-# 	x,adj,link_loss,ent_loss=dense_diff_pool(x,adj,s,mask)
+	x,adj,link_loss,ent_loss=dense_diff_pool(x,adj,s,mask)
 
-# 	assert x.size()==(batch_size,num_clusters,channels)
-# 	assert adj.size()==(batch_size,num_clusters,num_clusters)
-# 	assert link_loss.item()>=0
-# 	assert ent_loss.item()>=0
+	assert x.size()==(batch_size,num_clusters,channels)
+	assert adj.size()==(batch_size,num_clusters,num_clusters)
+	assert link_loss.item()>=0
+	assert ent_loss.item()>=0
 
 def test_Diff_Pool():
 	from datasets.SMT import SMTDataLoader
 
 	smt=SMTDataLoader(config)
 	dataset=smt.get_dataset('SMT',sparse=config.sparse,dataset_div=config.dataset_div)
-	model=DiffPool(dataset,config.num_layers,config.hidden)
+	model=DiffPool(dataset,config.num_pools,config.hidden)
 	model.eval()
 	for batch in smt.train_loader:
 		assert list(batch.x.size())[1:]==[smt.num_nodes,dataset[0].num_features]
