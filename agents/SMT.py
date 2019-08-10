e90e980cb668709e637766e2c9ec49b0d7e628ce
pwycl <814821479@qq.com>
2019-08-10 10:22:05 +0800

    Update SMT.py

--- agents/SMT.py
+++ agents/SMT.py
@@ -62,7 +62,7 @@ class SMTAgent(BaseAgent):
         self.current_iteration = 0
         self.best_val_acc = 0
 
-    def load_checkpoint(self, file_name='best_model_best.pth.tar'):
+    def load_checkpoint(self, file_name='best_checkpoint.pth.tar'):
         """
         Latest checkpoint loader
         :param file_name: name of the checkpoint file
